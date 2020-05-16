b26376ad047c0cd8fc10921a0cf7ee097a79d0ed
David Aviles <davidavilesco@gmail.com>
2020-05-15 18:01:28 -0600

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,20 @@
 // ======================
 
 process.env.PORT = process.env.PORT || 3000;
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+// =======================
+// Base de datos
+// ======================
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://strider:A4NGicVmL3v397Lq@cluster0-6o9tz.mongodb.net/cafe';
+}
+
+process.env.URLDB = urlDB;
